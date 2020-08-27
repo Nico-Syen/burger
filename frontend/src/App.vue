a4a642c43d5291f0c7c386f8ad0198e005e81eb7
Nico.Syen <Nicolas.Lorthioir@gmail.com>
2020-08-27 17:42:51 +0200

    router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/burger.jpg">
-    <ConnexionForm msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/admin">Admin</router-link>
+    </div>
+    <router-view/>
   </div>
-
-
 </template>
-
-<script>
-import ConnexionForm from './components/ConnexionForm.vue'
-
-export default {
-  name: 'App',
-  components: {
-    ConnexionForm
-  }
-}
-</script>
 
 <style>
 #app {
@@ -25,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ frontend/src/views/Admin.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="admin">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+<script>
+// import ConnexionForm from '../components/ConnexionForm.vue'
+
+export default {
+  name: 'Admin',
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
